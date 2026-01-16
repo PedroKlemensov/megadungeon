ae1fc04142ea7c1df06c44a75c719851c6d150ef
Pedro klemensov <pedroklemensov@gmail.com>
2026-01-16 01:42:23 -0300

    mecanica de pulo em paredes

--- Scripts/Jogador/jogadorS.gd
+++ Scripts/Jogador/jogadorS.gd
@@ -2,8 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 10.0
-const JUMP_VELOCITY = SPEED * 1
-
+const JUMP_VELOCITY = 9
 
 
 func _physics_process(delta: float) -> void:
@@ -14,7 +13,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_wall():
+		velocity.y = JUMP_VELOCITY
+		  
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
